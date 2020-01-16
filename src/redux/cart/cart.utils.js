cb498cbcedf116e26c63d9e0beffd4ccca65ff3f
alanbed <alanjbedoya@gmail.com>
2020-01-15 21:03:35 -0800

    edited my cart.utils and connected items to be shown in cart dropdown.

--- src/redux/cart/cart.utils.js
+++ src/redux/cart/cart.utils.js
@@ -1,14 +1,16 @@
 export const addItemToCart = (cartItems, cartItemToAdd) => {
-    const existingCartItem = cartItems.find(cartItem => cartItem.id === cartItemToAdd.id);
-
-    if(existingCartItem) {
-        return cartItems.map(cartItem => 
-            cartItem.id === cartItemToAdd.id 
-                ? { ...cartItem, quantity: cartItem.quanity + 1 }
-                : cartItem
-            );
+    const existingCartItem = cartItems.find(
+      cartItem => cartItem.id === cartItemToAdd.id
+    );
+  
+    if (existingCartItem) {
+      return cartItems.map(cartItem =>
+        cartItem.id === cartItemToAdd.id
+          ? { ...cartItem, quantity: cartItem.quantity + 1 }
+          : cartItem
+      );
     }
-
-    return [...cartItems, { ...cartItemToAdd, quanity: 1 }];
-};
+  
+    return [...cartItems, { ...cartItemToAdd, quantity: 1 }];
+  };
 
